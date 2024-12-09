098f06caa223c13926805a3b1c4d9e91b6879560
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 13:24:18 +0330

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -35,8 +35,25 @@ export default [
 
       // TypeScript-Specific Rules
       '@typescript-eslint/explicit-function-return-type': ['error'], // Require explicit return type for functions and methods
-
+    }
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,vue}'],
+    languageOptions: { globals: globals.browser },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+    rules: {
       // Vue-Specific Rules
+      'vue/multi-word-component-names': 'off', // Disable multi-word component name rule for Vue files
       'vue/html-indent': ['error', 2], // Enforce 2-space indent in templates
       'vue/singleline-html-element-content-newline': 'off', // Don't enforce newlines for single-line HTML elements
       'vue/multiline-html-element-content-newline': 'error', // Require newlines for multi-line HTML elements
@@ -49,23 +66,6 @@ export default [
       ],
       'vue/require-default-prop': 'off', // Disable required default props for Vue components
       'vue/no-multiple-template-root': 'off', // Allow multiple root nodes in Vue templates
-    }
-  },
-  {
-    files: ['**/*.{js,mjs,cjs,ts,vue}'],
-  },
-  {
-    languageOptions: { globals: globals.browser },
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-  {
-    files: ['**/*.vue'],
-    languageOptions: {
-      parserOptions: {
-        parser: tseslint.parser,
-      },
     },
   },
 ];
