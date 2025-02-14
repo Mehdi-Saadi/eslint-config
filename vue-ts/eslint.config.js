e629959881c0eb70e0d5f3c2cb19732f3d68ecfa
Mehdi <mehdi.0.saadi@gmail.com>
2025-02-14 16:52:43 +0330

    update config

--- vue-ts/eslint.config.js
+++ vue-ts/eslint.config.js
@@ -5,44 +5,42 @@ import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     rules: {
-      'semi': ['error', 'always'], // Always require semicolons
-      'quotes': ['error', 'single', { avoidEscape: true }], // Prefer single quotes
-      'indent': ['error', 2], // Enforce 2-space tab width
+      'semi': ['warn', 'always'], // Always require semicolons
+      'quotes': ['warn', 'single', { avoidEscape: true }], // Prefer single quotes
 
       // Possible Problems
-      'no-duplicate-imports': ['error'],
-      'no-template-curly-in-string': ['error'],
-      'no-use-before-define': ['error'],
-      'no-useless-assignment': ['error'],
-      'require-atomic-updates': ['error'],
+      'no-duplicate-imports': ['warn'],
+      'no-template-curly-in-string': ['warn'],
+      'require-atomic-updates': ['warn'],
 
       // Suggestions
-      'arrow-body-style': ['error', 'as-needed'],
-      'block-scoped-var': ['error'],
-      'curly': ['error'], // Always use curly braces in control statements
-      'default-case': ['error'], // Require a default case in switch statements
-      'default-case-last': ['error'], // Enforce default case to be the last
-      'default-param-last': ['error'],
-      'eqeqeq': ['error'], // Require strict equality (===)
-      'func-style': ['error', 'expression'],
-      'grouped-accessor-pairs': ['error', 'getBeforeSet'],
-      'no-else-return': ['error'],
-      'no-empty-function': ['error'],
-      'no-lonely-if': ['error'],
-      'prefer-const': ['error'],
+      'arrow-body-style': ['warn', 'as-needed'],
+      'block-scoped-var': ['warn'],
+      'curly': ['warn'], // Always use curly braces in control statements
+      'default-case-last': ['warn'], // Enforce default case to be the last
+      'default-param-last': ['warn'],
+      'eqeqeq': ['warn'], // Require strict equality (===)
+      'func-style': ['warn', 'expression'],
+      'grouped-accessor-pairs': ['warn', 'getBeforeSet'],
+      'no-else-return': ['warn'],
+      'no-empty-function': ['warn'],
+      'no-lonely-if': ['warn'],
+      'prefer-const': ['warn'],
 
       // TypeScript-Specific Rules
-      '@typescript-eslint/explicit-function-return-type': ['error'], // Require explicit return type for functions and methods
+      '@typescript-eslint/explicit-function-return-type': ['warn'], // Require explicit return type for functions and methods
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      '@typescript-eslint/no-explicit-any': ['warn'],
     }
   },
   {
     files: ['**/*.{js,mjs,cjs,ts,vue}'],
     languageOptions: { globals: globals.browser },
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   {
     files: ['**/*.vue'],
@@ -53,19 +51,19 @@ export default [
     },
     rules: {
       // Vue-Specific Rules
-      'vue/multi-word-component-names': 'off', // Disable multi-word component name rule for Vue files
-      'vue/html-indent': ['error', 2], // Enforce 2-space indent in templates
-      'vue/singleline-html-element-content-newline': 'off', // Don't enforce newlines for single-line HTML elements
-      'vue/multiline-html-element-content-newline': 'error', // Require newlines for multi-line HTML elements
+      'vue/multi-word-component-names': ['off'], // Disable multi-word component name rule for Vue files
+      'vue/html-indent': ['warn', 2], // Enforce 2-space indent in templates
+      'vue/singleline-html-element-content-newline': ['off'], // Don't enforce newlines for single-line HTML elements
+      'vue/multiline-html-element-content-newline': ['warn'], // Require newlines for multi-line HTML elements
       'vue/max-attributes-per-line': [
-        'error',
+        'warn',
         {
           singleline: 1, // One attribute per line for single-line elements
           multiline: 1, // One attribute per line for multi-line elements
         },
       ],
-      'vue/require-default-prop': 'off', // Disable required default props for Vue components
-      'vue/no-multiple-template-root': 'off', // Allow multiple root nodes in Vue templates
+      'vue/require-default-prop': ['off'], // Disable required default props for Vue components
+      'vue/no-multiple-template-root': ['off'], // Allow multiple root nodes in Vue templates
     },
   },
 ];
